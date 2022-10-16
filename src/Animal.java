914000a65f346db59dd4992bc70f610a8cb97ad7
maltsev-as <62901280+maltsev-as@users.noreply.github.com>
2022-10-16 20:10:11 +0300

    test update
    test update

--- src/Animal.java
+++ src/Animal.java
@@ -2,7 +2,7 @@ public class Animal {
     String name = "Some animal";
 
     public void eat (){
-        System.out.println("I am eating");
+        System.out.println("I am eating"); //test
     }
     public void sleep(){
         System.out.println("I am sleeping");
